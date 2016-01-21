d164b3100db84b490f094f412aa2b1f5ee759200
Adam Rackis <adamrackis@gmail.com>
2016-01-20 23:42:50 -0600

    react installed and working

--- config.js
+++ config.js
@@ -17,6 +17,8 @@ System.config({
     "babel": "npm:babel-core@5.8.35",
     "babel-runtime": "npm:babel-runtime@5.8.35",
     "core-js": "npm:core-js@1.2.6",
+    "react": "npm:react@0.14.6",
+    "react-dom": "npm:react-dom@0.14.6",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
@@ -41,6 +43,9 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
+    "npm:fbjs@0.6.1": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
@@ -49,6 +54,13 @@ System.config({
     },
     "npm:process@0.11.2": {
       "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:react-dom@0.14.6": {
+      "react": "npm:react@0.14.6"
+    },
+    "npm:react@0.14.6": {
+      "fbjs": "npm:fbjs@0.6.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
---
+++ demos/reactComponent1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponent extends React.Component {
+    render() {
+        return <div>Hello World</div>;
+    }
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById('root'));
+
+export default {}
