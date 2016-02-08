f65d9f46a09b4b3d17500510bb3b68c43e420e66
Adam Rackis <adamrackis@gmail.com>
2016-02-07 22:16:36 -0600

    step 1 code written

---
+++ app/component.js
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import { connect } from 'react-redux';
+
+class Main extends React.Component {
+    increment(){
+        this.props.dispatch({ type: 'INCREMENT' });
+    }
+    decrement(){
+        this.props.dispatch({ type: 'DECREMENT' });
+    }
+    render(){
+        return (
+            <div>{'Value: ' + this.props.value} <button onClick={() => this.increment()}>+</button><button onClick={() => this.decrement()}>-</button></div>
+        );
+    }
+}
+
+const ConnectedMain = connect(state => state)(Main);
+
+export default ConnectedMain;
---
+++ snapshots/step1_SillyCounter/store.js
@@ -0,0 +1,17 @@
+﻿import { createStore } from 'redux';
+
+const initialState = {
+    value: 0
+};
+
+function reducer(state = initialState, action){
+    switch(action.type){
+        case 'INCREMENT':
+            return Object.assign({}, state, { value: state.value + 1 });
+        case 'DECREMENT':
+            return Object.assign({}, state, { value: state.value - 1 });
+    }
+    return state;
+}
+
+export default createStore(reducer);
