3d27498e19ae0549bef6e144b9cb3bb808e649e2
Adam Rackis <adamrackis@gmail.com>
2016-02-10 23:57:57 -0600

    step 2 - books in grid

---
+++ data/subjects.js
@@ -0,0 +1,8 @@
+export default [
+    { _id: 1, name: 'JavaScript' },
+    { _id: 2, name: 'ES6' },
+    { _id: 3, name: 'MongoDB' },
+    { _id: 5, name: 'Functional Programming' },
+    { _id: 10, name: 'Misc Dev' },
+    { _id: 99, name: 'Trolling' }
+];
