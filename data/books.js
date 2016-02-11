72adf8457bf511780cd89705a65c0fb4333c5833
Adam Rackis <adamrackis@gmail.com>
2016-02-11 00:44:53 -0600

    book selection added for step 3

--- data/books.js
+++ data/books.js
@@ -4,5 +4,5 @@ export default [
     { _id: 3, title: 'JavaScript Allongé', author: 'Reg Braithwaite', subjects: [1, 5] },
     { _id: 4, title: 'JavaScript the Good Parts', author: 'Douglas Crockford', subjects: [99] },
     { _id: 5, title: 'Mongo DB the Definitive Guide', author: 'Kristina Chodorow', subjects: [3] },
-    { _id: 6, title: 'Seven Languages in Seven Weeks', author: 'Bruce A. Tate', subjects: [10] },
+    { _id: 6, title: 'Seven Languages in Seven Weeks', author: 'Bruce A. Tate', subjects: [10] }
 ];
