7cd08797bf399f5889d4c4e6d0e7916b9e5c2592
Adam Rackis <adamrackis@gmail.com>
2016-02-14 20:25:18 -0600

    small tweak

--- snapshots/step3_booksWithSelecting/toggleImplementation.js
+++ snapshots/step3_booksWithSelecting/toggleImplementation.js
@@ -4,7 +4,7 @@
             let bookToUpdate = newBooks.find(b => b._id == action._id);
             let index = newBooks.indexOf(bookToUpdate);
 
-            newBooks[index] = Object.assign(bookToUpdate, { selected: !bookToUpdate.selected });
+            newBooks[index] = Object.assign({}, bookToUpdate, { selected: !bookToUpdate.selected });
 
             return Object.assign({}, state, { books: newBooks });
 
